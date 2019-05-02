177723784a4ac4ffd379e98d87417155af09f2c6
luci <mrluci1981@gmail.com>
2019-05-03 01:47:30 +0300

    Retete 97% final

--- core/views.py
+++ core/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categorii, Topics
 from .forms import NewTopicForm, NewCommentForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     categorii = Categorii.objects.all()
     return render(request, 'core/index.html', {'categorii': categorii})
 
-
+@login_required
 def topics(request, pk):
     # categorie = Categorii.objects.get(pk=pk)
     categorie = get_object_or_404(Categorii, pk=pk)
 
     return render(request, 'core/topics.html', {'categorie': categorie})
 
-
+@login_required
 def postari(request, pk, topic_pk):
     # topic = Topics.objects.get(categorie__pk=pk, pk=topic_pk)
     topic = get_object_or_404(Topics, categorie__pk=pk, pk=topic_pk)
     return render(request, 'core/discutii.html', {'topic': topic})
 
+
+@login_required
 def new_topic(request, pk):
     # categorie = Categorii.objects.get(pk=pk)
     categorie = get_object_or_404(Categorii, pk=pk)
@@ -35,7 +38,7 @@ def new_topic(request, pk):
     return render(request, 'core/reteta noua.html', {'categorie':categorie, 'form':form})
 
 
-
+@login_required
 def new_post(request, pk, topic_pk):
     topic = get_object_or_404(Topics, categorie__pk=pk, pk=topic_pk)
 
